52d524c61bf1f4bff0e128ec51dac294f1a30217
O2xy <O2xy@users.noreply.github.com>
2020-10-22 00:15:09 +0300

    Lesson5DZ

--- public/photo.php
+++ public/photo.php
@@ -1,2 +1,16 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/../config/main.php";
+require_once ENGINE_DIR . "gallerydb.php";
+
 $id = $_GET['id'];
+$host = '127.0.0.1';
+$login = 'root';
+$password = 'root';
+$db = 'phpicters';
+
+
+$image = getPhotobyID ($id, $host, $login, $password, $db);
+$imgUrl = "{$image[2]}";
+include VIEWS_DIR . "photo.php";
+?>
+
--- views/gallery.php
+++ views/gallery.php
@@ -1,9 +1,18 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<body class="row">
 <?php
-foreach ($images as $image) :
-    $imgUrl = "/imgs/{$image}";
+
+foreach ($namedImgs as $image) :
+    $imgUrl = "{$image['path']}";
 ?>
+
     <a href="/photo.php?id=<?=$image['id']?>" target='_blank'>
-            <img src="<?=$imgUrl?>" width='200'>
+            <img src="<?=$imgUrl?>" alt="<?=$image['name']?>" width='200'>
+        <p>
+            Название картинки: <?=$image['name']?> <br>
+            ID картинки в базе: <?=$image['id']?> <br>
+            Количество просмотров: <?=$image['views']?>
+        </p>
     </a>
 <? endforeach;?>
-
+</body>
